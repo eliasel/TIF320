d9f90c970571e91ba76f0edfc48a9c52cd4f440a
Elias Elmquist <eliasel@hebbe1.int.private>
2021-02-04 17:40:09 +0100

    done som nice files for T5

---
+++ HW2/Na-clusters-GA-search/7a/extract_groundstate.py
@@ -0,0 +1,22 @@
+from ase.db import connect
+from ase.io import write 
+
+lowest_energy = 0
+lowest_id = 0
+db = connect('gadb.db')
+
+for row in db.select():
+  atoms = row.toatoms()
+  print(atoms.calc.)
+  if(atoms.calc != None):
+    current_energy = row.energy
+    if current_energy<lowest_energy:
+      lowest_energy = current_energy
+      lowest_id = row.id
+
+atoms = db.get(id=lowest_id).toatoms()
+print(atoms)
+
+print(atoms.get_total_energy())
+write('ground_states.xyz', atoms)
+
