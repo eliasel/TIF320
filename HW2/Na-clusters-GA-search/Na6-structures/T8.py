bb7cc01998291b09c1c53fd8c5504ef720a9f6a5
eliasel <eeelmquist@gmail.com>
2021-02-05 16:43:19 +0100

    moving stuff back home

--- HW2/Na-clusters-GA-search/Na6-structures/T8.py
+++ HW2/Na-clusters-GA-search/Na6-structures/T8.py
@@ -1,20 +1,20 @@
 import numpy as np
 from ase import Atoms
 from ase.db import connect
-from ase.ga.data
 from ase.optimize import GPMin
 from ase.io import write 
 
-from gpaw import GPAW
+from gpaw import GPAW, FermiDirac
 import argparse
 
+parser = argparse.ArgumentParser()
 parser.add_argument('mode', help = 'set the mode for GPAW allowed modes fd, lcao or pw') 
 
 defval = 10
 parser.add_argument('-n', '--nbands', type=int, default=defval, help='nbands value for GPAW (default: {})'.format(defval))
 
 defval = 0.25
-parser.add_argument('-hval', '--hval', type=float, default=defval, help='Value of h in GPAW (default: {})'.format(defval))
+parser.add_argument( '--hval', type=float, default=defval, help='Value of h in GPAW (default: {})'.format(defval))
 
 args = parser.parse_args()
 
@@ -24,27 +24,27 @@ args = parser.parse_args()
 db = connect("atomisar.db")
 number_of_atom_clusters = db.count('natoms>0') 
 
-print(f'running in {args.mode} mode')
+#print(f'running in {args.mode} mode')
 #set calculator parameters
 if (args.mode == 'lcao'):
 
   calc = GPAW(nbands=args.nbands,
-            h=0.25,
+            h=args.hval,
             txt='out.txt',
             occupations=FermiDirac(0.05),
             setups={'Na': '1'},
             mode=args.mode,
             basis='dzp')
-if (args.mode ==  'ld'):
+if (args.mode ==  'fd'):
   calc = GPAW(nbands=args.nbands,
-            h=0.25,
+            h=args.hval,
             txt='out.txt',
             occupations=FermiDirac(0.05),
             setups={'Na': '1'},
             mode=args.mode)
 if (args.mode ==  'pw'):
   calc = GPAW(nbands=args.nbands,
-            h=0.25,
+            h=args.hval,
             txt='out.txt',
             occupations=FermiDirac(0.05),
             setups={'Na': '1'},
@@ -52,7 +52,7 @@ if (args.mode ==  'pw'):
 
 
 #Loop over all clusters and relax them
-for atom_cluster in db:
+for atom_cluster in db.select():
   atoms = atom_cluster.toatoms()
   atoms.calc = calc  
   #print(atoms) 
@@ -60,4 +60,4 @@ for atom_cluster in db:
   dynamics.run(fmax=0.02, steps=100)
   
   #Write minimized cluster to xyz datafile 
-  write('gs_{args.mode}_n{nbands}_.xyz', atoms)
+  write(f'gs_{args.mode}_n{nbands}_.xyz', atoms)
