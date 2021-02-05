03bc23f7266db6ce046f0e7d502ccd844caed9b5
Elias Elmquist <eliasel@hebbe1.int.private>
2021-02-05 11:43:46 +0100

    moving all to home

--- HW2/Na-clusters-GA-search/T3.py
+++ HW2/Na-clusters-GA-search/T3.py
@@ -1,22 +1,30 @@
 import numpy as np
 from ase import Atoms
 from ase.db import connect
-from ase.ga.data
 from ase.optimize import GPMin
+from ase.io import write
 
-
-from gpaw import GPAW
+from gpaw import GPAW, FermiDirac
 
 #connection to database
 db = connect("atomisar.db")
-number_of_atom_clusters = db.count('natoms>0') 
+#number_of_atom_clusters = db.count('natoms>0') 
 
-calc = GPAW(nbands=10, txt='out.txt', mode='lcao', basis='dzp')
+calc = GPAW(nbands=10,
+	    h=0.25,
+	    txt ='out.txt',
+	    occupations=FermiDirac(0.05),
+	    setups={'Na': '1'},
+	    mode='lcao',
+	    basis = 'dzp')
 
-for atom_cluster in db:
-  atoms = atom_cluster.toatoms()
+for atom_cluster_rows in db.select():
+  atoms = atom_cluster_rows.toatoms()
   atoms.calc = calc  
   #print(atoms) 
-  dynamics = GPMin(atoms)
+  dynamics = GPMin(atoms, trajectory = 'relax.traj', logfile ='log.log' )
   dynamics.run(fmax=0.02, steps=100)
+  energy = atoms.get_total_energy()
   
+  write('ground_states.xyz', atoms)
+
