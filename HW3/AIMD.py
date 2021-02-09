a53c657f6abd915d5b85ab01a49e4f3cfa600ee8
eliasel <eeelmquist@gmail.com>
2021-02-09 19:25:50 +0100

    Done some work on the

--- HW3/AIMD.py
+++ HW3/AIMD.py
@@ -1,5 +1,31 @@
 from ase.db import connect
 from ase.io import write
+from ase.units import fs, kB
+
+
+atoms = read('NaH20.xyz')
+
+calc = GPAW(
+            mode = 'lcao',
+            xc = 'PBE',
+            basis = 'dzp',
+            symmetry = {point_group = False},
+            charge = 1,
+            txt = 'GPAW_output.gpaw-out')
+
+atoms.set_calculator(calc)
+
+dyn = NPT(atom
+            temperature = 350*kB,
+            timestep = 0.5*fs,
+            ttime = 20*fs,
+            logfile = 'MDout.log')
+
+traj = Trajectory('cluster24wNa.tradj','w',atoms)
+dyn.attach(traj.write, interval = 1)
+
+dyn.run(10)
+
 
 """lowest_energy = 0
 lowest_id = 0
