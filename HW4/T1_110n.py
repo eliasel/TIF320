b68adcd61ec75fe684d836e2b99ee0d5f0e004af
eliasel <eeelmquist@gmail.com>
2021-02-18 12:29:20 +0100

    Update T1_110n.py

--- HW4/T1_110n.py
+++ HW4/T1_110n.py
@@ -4,10 +4,14 @@ from gpaw import GPAW
 
 # Reload ground state from before to
 calc = GPAW("gsNa8.gpw")
+calc.get_eigenvalues()
+calc.get_occupation_numbers()
+
 atoms = calc.get_atoms()
+atoms.get_potential_energy() # Performs the calculation on the system !
 
 calc.set(
-nbands = 110,           #
+nbands = 10,           #
 convergence = {"bands", -10},# Do not converge final 10 states
 fixdensity = True,      # Keep density constant in calculations
 txt = "Na_n110.gpaw-out"     # Redirect GPAW ’s text output to f.gpaw - out
