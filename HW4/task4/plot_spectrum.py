b3fa3b1c12a2833eacb6f60b529a1397a6483352
eliasel <eeelmquist@gmail.com>
2021-02-19 18:07:10 +0100

    Done w T4

---
+++ HW4/task4/plot_spectrum.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.genfromtxt("be_spectrum_x.dat")
+y = np.genfromtxt("be_spectrum_y.dat")
+z = np.genfromtxt("be_spectrum_z.dat")
+
+avg = (x[:,1]+y[:,2]+z[:,3])/3
+
+plt.plot(x[:,0],avg)
+
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(x[:,0],avg, label ='photoabsorption spectrum')   
+ax.set_xlim([0, 6.5])
