1f0766d6fef286ef0de6e46ac4ee63f07313af4e
Erik Svensson <esve@hebbe2.int.private>
2021-02-19 17:16:07 +0100

    Nyckelpiga

---
+++ HW4/task5/plt.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from gpaw.lrtddft import LrTDDFT
+from gpaw.lrtddft.spectrum import get_folded_spectrum
+
+lr = LrTDDFT('LrTDDFTresults.dat') 
+lr.diagonalize(energy_range=4) 
+
+x,y = get_folded_spectrum(lr, width = 0.06)
+fig, ax = plt.subplots()
+plt.plot(x, y[:,0])
+ax.set_xlabel('energy [eV]')
+plt.savefig('./spectrum_T5.pdf')
