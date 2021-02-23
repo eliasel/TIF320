006c488322807df017f3462d4c98a4f435afa6e0
eliasel <eeelmquist@gmail.com>
2021-02-23 14:21:04 +0100

    Print T2 to file

--- HW5/T2 - Copy.py
+++ HW5/T2 - Copy.py
@@ -8,10 +8,9 @@ import numpy as np
 atoms = bulk('Al')
 calc = EAM(potential = 'al_potential.alloy')
 atoms.calc = calc
-
-f = open("energy_v_lattice.txt", "w")
-f.write("lattice, energy\n")
-
+#pot_E = atoms.get_potential_energy()
+#print(f'cohesive energy for bulk Al: {pot_E} eV?')
+#print(atoms.cell)
 start_cell = atoms.cell/2.025
 a = np.linspace(1.5,2.5,4000)
 energies = np.zeros(4000)
@@ -19,6 +18,5 @@ i=0
 for a in a:
     atoms.set_cell(start_cell*a)
     energies[i] = atoms.get_potential_energy()
-    print(f'{start_cell/2.025*a} energy :  {energies[i]}')
-    f.write(f"{start_cell/2.025*a}, {energies[i]}\n")
+    print(f'{start_cell/2.025*a} energy :  {energies[i]})
     i += 1
