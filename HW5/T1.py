eea074843851e53e662bf1297fb8e71b11f8e7c1
eliasel <eeelmquist@gmail.com>
2021-02-23 12:04:30 +0100

    started on T2

--- HW5/T1.py
+++ HW5/T1.py
@@ -1,6 +1,5 @@
 from ase import atoms
 from ase.io import read, write
-#from gpaw import GPAW
 from ase.db import connect
 from ase.calculators.eam import EAM
 
@@ -8,8 +7,10 @@ from ase.calculators.eam import EAM
 #connect to database and retrieve atoms object
 db = connect("Al-clusters-initial.db")
 calc = EAM(potential = 'al_potential.alloy')
-for clusters in db:
+i=0
+for clusters in db.select():
     atoms = clusters.toatoms()
     atoms.calc = calc
     pot_E = atoms.get_potential_energy()
-    print(pot_E)
+    print(f'cohesive energy for index {i}: {pot_E} eV?')
+    i += 1
