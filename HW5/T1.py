40f223f2ef9e16df59b831be46854fee15133f9e
eliasel <eeelmquist@gmail.com>
2021-02-23 15:34:08 +0100

    Update T1.py

--- HW5/T1.py
+++ HW5/T1.py
@@ -8,9 +8,10 @@ from ase.calculators.eam import EAM
 db = connect("Al-clusters-initial.db")
 calc = EAM(potential = 'al_potential.alloy')
 i=0
-for clusters in db.select():
-    atoms = clusters.toatoms()
+for cluster in db.select():
+    atoms = cluster.toatoms()
+    No_atoms = len(atoms)
     atoms.calc = calc
     pot_E = atoms.get_potential_energy()
-    print(f'cohesive energy for index {i}: {pot_E} eV?')
+    print(f'cohesive energy for index {i}: {pot_E/No_atoms} eV?')
     i += 1
