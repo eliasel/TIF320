bdca172758f90198f88d43ead7c615d250a75c2b
Dushq <esve@student.chalmers.se>
2021-02-24 04:05:04 +0100

    askdjfhawq

---
+++ HW5/task3/T3.py
@@ -0,0 +1,29 @@
+from ase import atoms
+from ase.io import read, write
+from ase.db import connect
+from ase.calculators.eam import EAM
+from ase.optimize import BFGS
+from ase.vibrations import Vibrations
+
+
+#connect to database and retrieve atoms object
+db = connect("Al-clusters-initial.db")
+calc = EAM(potential = 'al_potential.alloy')
+i=1
+#for cluster in db.select():
+   # atoms = cluster.toatoms()
+atoms = db[1].toatoms()
+No_atoms = len(atoms)
+atoms.calc = calc
+BFGS(atoms).run(fmax=0.01)
+vib = Vibrations(atoms)
+vib.run()
+vib.write_dos(out='vib-dos'+str(i)+'.dat')
+freq = vib.get_frequencies()
+print(vib.get_mode(1))
+print(freq)
+#i=i+1
+
+
+
+#db.write(atoms, data = {'frequency' : freq, 'density-of-states' : dos})
---
+++ HW5/task5/T5.py
@@ -0,0 +1,32 @@
+from ase import atoms
+from ase.db import connect
+from ase.io import write, read
+from ase.calculators.eam import EAM
+from ase.dft.dos import DOS
+#from gpaw import GPAW
+
+db = connect('Al-clusters-initial.db')
+calc = EAM(potential = 'al_potential.alloy')
+
+#calc = EAM('al_potential.alloy')
+#calc = GPAW(
+#mode ='fd',
+#xc = 'LDA',
+#setups = {'Na':  '1'},
+#nbands=0,
+#txt='T5.gpaw-out'
+#)
+
+d = ()
+e = ()
+for cluster in db.select():
+    atoms = cluster.toatoms()
+    if atoms.get_global_number_of_atoms()<100.0:
+        atoms.set_calculator(calc)
+        pot_E = atoms.get_potential_energy()
+        dos = DOS(calc)
+        d = d + (dos.get_dos(),)
+        e = e + (dos.get_energies(),)
+
+
+
