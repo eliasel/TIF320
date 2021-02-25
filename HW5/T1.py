869158c72e6e2f255c47e0744d352b9c60601ff1
eliasel <eeelmquist@gmail.com>
2021-02-25 14:32:24 +0100

    Added relaxation

--- HW5/T1.py
+++ HW5/T1.py
@@ -2,6 +2,7 @@ from ase import atoms
 from ase.io import read, write
 from ase.db import connect
 from ase.calculators.eam import EAM
+from ase.optimize import BFGS
 
 
 #connect to database and retrieve atoms object
@@ -10,7 +11,8 @@ calc = EAM(potential = 'al_potential.alloy')
 i=0
 for cluster in db.select():
     atoms = cluster.toatoms()
-    No_atoms = len(atoms)
+    BFGS(atoms).run(fmax=0.01)
+    N_atoms = len(atoms)
     atoms.calc = calc
     pot_E = atoms.get_potential_energy()
     print(f'cohesive energy for index {i}: {pot_E/No_atoms} eV?')
--- HW5/T2.py
+++ HW5/T2.py
@@ -4,6 +4,8 @@ from ase.db import connect
 from ase.calculators.eam import EAM
 from ase.build import bulk
 import numpy as np
+from ase.optimize import BFGS
+
 
 atoms = bulk('Al')
 calc = EAM(potential = 'al_potential.alloy')
@@ -20,6 +22,7 @@ min_energy = 10
 best_a = 0
 for a in a:
     atoms.set_cell(start_cell*a)
+    BFGS(atoms).run(fmax=0.01)
     energies[i] = atoms.get_potential_energy()
     #Use BFGS to minimize the structure?
 
