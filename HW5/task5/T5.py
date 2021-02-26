2f4325efebce8f8e8e296fa11263215614c687dc
eliasel <>
2021-02-26 17:40:15 +0100

    Update T5.py

--- HW5/task5/T5.py
+++ HW5/task5/T5.py
@@ -5,17 +5,31 @@ from ase.calculators.eam import EAM
 from ase.optimize import BFGS
 from ase.vibrations import Vibrations
 from gpaw import GPAW, FermiDirac, PW
+
+import argparse
+
+parser = argparse.ArgumentParser()
+defval = 2
+parser.add_argument('-n', '--ID', type=int, default=defval,
+                    help='ID of cluster 1 to 6 (default: {:.2f})'.format(defval))
+defval = 4
+parser.add_argument('-k', '--kpt', type=int, default=defval,
+                    help='K value for kpts (default: {:.2f})'.format(defval))
+
+args = parser.parse_args()
+
 #connect to database and retrieve atoms object
 db = connect("sub_100_Al-relaxed.db")
 #sub_100_ids=[2, 3, 6, 7, 8, 10]
 
-sub_100_ids = [2]
-k = 4
+sub_100_ids = [args.ID]
+k = args.kpt
 calc = GPAW(mode=PW(300),
             xc='PBE',
             kpts=(k, k, k),
             random=True,  # random guess (needed if many empty bands required)
             occupations=FermiDirac(0.01))
+
 
 for cluster in db.select():
     atoms = cluster.toatoms()
@@ -28,6 +42,6 @@ for cluster in db.select():
         nbands=16,
         symmetry='off',
         kpts={'path': 'GXWKL', 'npoints': 60},
-        convergence={'bands': 8}
+        convergence={'bands': 8})
     Bs = calc.band_structure()
-    bs.plot(filename='bandstructure.png', show=True, emax=10.0)
+    bs.plot(filename='band_ID:'+str(args.ID)+'_k:'+str(k)+'.png', show=True, emax=10.0)
