d78f46a2185c88e622ddb602077282cb6039f260
eliasel <>
2021-02-26 23:43:28 +0100

    Update DOSY5.py

--- HW5/task5/DOSY5.py
+++ HW5/task5/DOSY5.py
@@ -41,24 +41,8 @@ print('band structure:')
 bs = calc.band_structure()
 
 #bs.plot(filename='k:'+str(k)+'bulk.png', show=False, emax=10.0)
-
-dos = ph.get_dos(kpts=(8, 8, 8)).sample_grid(npts=60, width=None)
-
-# Plot the band structure and DOS:
-import matplotlib.pyplot as plt  # noqa
-fig = plt.figure(1, figsize=(7, 4))
-ax = fig.add_axes([.12, .07, .67, .85])
-
-emax = 0.035
-bs.plot(ax=ax, emin=0.0, emax=emax)
-
-dosax = fig.add_axes([.8, .07, .17, .85])
-dosax.fill_between(dos.weights[0], dos.energy, y2=0, color='grey',
-                   edgecolor='k', lw=1)
-
-dosax.set_ylim(0, emax)
-dosax.set_yticks([])
-dosax.set_xticks([])
-dosax.set_xlabel("DOS", fontsize=18)
-
-fig.savefig('Al_phonon.png')
+dos = DOS(calc, npts=500, width=0)
+energies = dos.get_energies()
+weights = dos.get_dos()
+plt.plot(energies, weights)
+plt.savefig('DOSAlBulk.png;')
