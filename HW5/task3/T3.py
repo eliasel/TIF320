38ab767e8a710a4b2c3ef9aaed1009ef8a358a2e
eliasel <eeelmquist@gmail.com>
2021-02-26 12:27:21 +0100

    tried to make T3 work

--- HW5/task3/T3.py
+++ HW5/task3/T3.py
@@ -9,21 +9,34 @@ from ase.vibrations import Vibrations
 db = connect("Al-clusters-initial.db")
 calc = EAM(potential = 'al_potential.alloy')
 i=1
-#for cluster in db.select():
-   # atoms = cluster.toatoms()
-atoms = db[i].toatoms()
-No_atoms = len(atoms)
-atoms.calc = calc
-BFGS(atoms).run(fmax=0.01)
-vib = Vibrations(atoms)
-vib.run()
-vib.write_dos(out='vib-dos'+str(i)+'.dat')
-freq = vib.get_frequencies()
-print(vib.get_mode(1))
-print(freq)
-print(len(freq))
-#i=i+1
+
+for cluster in db.select():
+#while (i == 1):
+#    cluster = db[2]
+    f = open('vib_spec_'+str(i)+'.txt','w')
+    f.write('frequency [cm^-1], energy [eV]\n')
+    atoms = cluster.toatoms()
+#atoms = db[i].toatoms()
+    N_atoms = len(atoms)
+    atoms.calc = calc
+    BFGS(atoms).run(fmax=0.01)
+    vib = Vibrations(atoms)
+    vib.clean()
+    vib.run()
+    vib.summary()
+#vib.write_dos(out='vib-dos'+str(i)+'.dat')
+    freq = vib.get_frequencies()
+#print(vib.get_mode(1))
+    #print(freq)
+#print(len(freq))
+    i=i+1
+    energies = vib.get_energies()
+    #print(energies)
+    print(len(freq))
+    for j in range(len(freq)):    
+        f.write(f'{freq[j]}, {energies[j]}\n')
+    f.close()    
+    vib.clean()
 
 
-
-db.write(atoms, data = {'frequency' : freq, 'density-of-states' : dos})
+#db.write(atoms, data = {'frequency' : freq, 'density-of-states' : dos})
